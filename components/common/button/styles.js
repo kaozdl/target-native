8f1c1dc746d870e0daa37762d384ce39086e422b
Kalil de Lima <kalil@rootstrap.com>
2019-02-19 11:54:57 -0300

    fix wrong indentation and add config file

--- components/common/button/styles.js
+++ components/common/button/styles.js
@@ -1,14 +1,14 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../../config';
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: 'black',
-        padding: 10,
-    },
-    text: {
-        color: 'white',
-    },
-    
+  button: {
+    backgroundColor: colors.black,
+    padding: 10,
+  },
+  text: {
+    color: colors.white,
+  },
 });
 
 export default styles;
--- components/common/input/styles.js
+++ components/common/input/styles.js
@@ -1,19 +1,19 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../../config';
 
 const styles = StyleSheet.create({
-    input: {
-        borderColor: 'black',
-        borderWidth: 1,
-        width: 220,
-        padding: 10,
-        textAlign: 'center'
-
-    },
-    label: {
-        color: 'black',
-        fontSize: 12,
-        textAlign: 'center'
-    },
+  input: {
+    borderColor: colors.black,
+    borderWidth: 1,
+    width: 220,
+    padding: 10,
+    textAlign: 'center'
+  },
+  label: {
+    color: colors.black,
+    fontSize: 12,
+    textAlign: 'center'
+  },
 });
 
 export default styles;
