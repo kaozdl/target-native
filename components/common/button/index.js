0316667c776a5306143d20fd9e429bec199a2c43
Kalil de Lima <kalil@rootstrap.com>
2019-02-21 11:00:50 -0300

    Remove unecessary View component from button

--- components/common/button/index.js
+++ components/common/button/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {Text, View, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
-const Button = ({name, action}) => (
-  <View>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={action}
-    >
-      <Text style={styles.text}>{name}</Text>
-    </TouchableOpacity>
-  </View>
+const Button = ({ name, action }) => (
+  <TouchableOpacity
+    style={styles.button}
+    onPress={action}
+  >
+    <Text style={styles.text}>{name}</Text>
+  </TouchableOpacity>
 );
 
 export default Button;
