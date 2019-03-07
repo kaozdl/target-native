5e6b3342ebd91238928c75127708299738d27ebf
Kalil de Lima <kalil@rootstrap.com>
2019-03-07 11:04:53 -0300

    install react-native-navigation it renders the main screen of the app

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
-
-import {AppRegistry} from 'react-native';
+import { Navigation } from "react-native-navigation";
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`MainApp`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "MainApp"
+      }
+    }
+  });
+});
